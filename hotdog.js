859c86a414f71019aedda71e47340cfcf331db46
Nataliya Tahova <ntahova@gmail.com>
2024-05-06 17:21:50 +0200

    DU01

---
+++ hotdog.js
@@ -0,0 +1,11 @@
+const toppings = [
+    { name: 'Hořčice', price: 5, selected: false },
+    { name: 'Kečup', price: 5, selected: false },
+    { name: 'Cibule', price: 5, selected: false },
+    { name: 'Okurka', price: 5, selected: false },
+    { name: 'Paprika', price: 5, selected: false },
+    { name: 'Rajče', price: 5, selected: false },
+    { name: 'Chilli', price: 5, selected: false },
+    { name: 'Sýr', price: 10, selected: false },
+    { name: 'Slanina', price: 10, selected: false },
+];
